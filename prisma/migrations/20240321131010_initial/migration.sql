75c37a5ee926a8695c3bc4e9a931ee561ba825c4
martinalmeida <martinalmeida56@gmail.com>
2024-03-21 13:18:16 -0500

    fix: :bug: migrations

--- prisma/migrations/20240321131010_initial/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `phone` on the `patients` table. The data in that column could be lost. The data in that column will be cast from `Int` to `Decimal(65,30)`.
-
-*/
--- AlterTable
-ALTER TABLE `patients` MODIFY `phone` DECIMAL(65, 30) NOT NULL;
--- prisma/migrations/20240321135457_initial/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `patients` MODIFY `num_doc` VARCHAR(191) NOT NULL,
-    MODIFY `phone` VARCHAR(191) NOT NULL;
